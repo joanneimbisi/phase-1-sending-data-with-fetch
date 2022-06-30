47aefc0c09de5f0b49786fe09d8fcd3dd78a736f
Joanne Imbisi <evizengwa@yahoo.com>
2022-06-30 11:48:21 -0700

    commit tree 5d37acc3bf6cf4a65d8ea0750cc7e9c4d96be92d

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+
+ function submitData (name, email){
+     console.log(name,email)
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        }
+        )}
+    
+     return fetch('http://localhost:3000/users',configurationObject)
+      .then(response => response.json())
+      .then(object => document.body.innerHTML = object.id)  
+
+.catch(function (error) { 
+    let message = 'Unauthorized Access'
+    document.body.innerHTML = error.message
+}
+)}
